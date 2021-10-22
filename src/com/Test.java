a5ae4d84bba28134f5c38ef759f716c4201cd065
DYY-Drummer <574861479@qq.com>
2021-10-22 19:52:39 +0800

    Update Test.java

--- src/com/Test.java
+++ src/com/Test.java
@@ -15,22 +15,22 @@ public class Test {
             String temp;
             //separate token
             while((temp=reader.readLine())!=null) {
-                //System.out.print("------"+temp+"------\n");
-                analysis.analyze(temp);
+                System.out.print("------"+temp+"------\n");
+                //analysis.analyze(temp);
             }
             Analysis.token_list.add(new Token("END","#"));
 //            for(int i=0;i<Analysis.token_list.size();i++){
 //                System.out.printf("%s  %s\n",Analysis.token_list.get(i).id,Analysis.token_list.get(i).word);
 //            }
             //parser
-            analysis.CompUnit();
+            //analysis.CompUnit();
             //close file
             reader.close();
         }catch (IOException e){
             System.out.println(e);
         }catch (Exception e1){
             System.out.println(e1);
-            //System.exit(1);
+            System.exit(1);
         }
 
 
